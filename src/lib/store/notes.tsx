d8d6bb3a2463db1a46e003356b1cc1d0b1a206a7
adoublef <kristopherab@gmail.com>
2023-06-20 18:24:51 +0100

    2 files modified

--- src/lib/store/notes.tsx
+++ src/lib/store/notes.tsx
@@ -1,24 +1,27 @@
-import { JSXElement, createContext, useContext, } from "solid-js";
+import { JSXElement, createContext, useContext } from 'solid-js';
+import { createStore } from 'solid-js/store';
 
-const NotesContext = createContext<[
-    { notes?: string[]; },
-]>();
+const NotesContext = createContext<[{ notes?: string[] }]>();
 
 export function NotesProvider(props: {
-    children: JSXElement;
-    notes: string[] | undefined;
+  children: JSXElement;
+  notes: string[] | undefined;
 }) {
-    return (
-        <NotesContext.Provider value={[{ notes: props.notes }]}>
-            {props.children}
-        </NotesContext.Provider>
-    );
+  const [store] = createStore({
+    notes: props.notes,
+  });
+
+  return (
+    <NotesContext.Provider value={[store]}>
+      {props.children}
+    </NotesContext.Provider>
+  );
 }
 
 export function useNotes() {
-    const context = useContext(NotesContext);
-    if (!context) {
-        throw new Error("useNotes must be used within a NotesProvider");
-    }
-    return context;
+  const context = useContext(NotesContext);
+  if (!context) {
+    throw new Error('useNotes must be used within a NotesProvider');
+  }
+  return context;
 }
--- src/routes/notes.tsx
+++ src/routes/notes.tsx
@@ -1,20 +1,19 @@
-import { Show, createEffect, createResource, createSignal, onMount } from "solid-js";
-import { Outlet, createRouteData, refetchRouteData, useRouteData } from "solid-start";
-import { NotesProvider } from "~/lib/store/notes";
+import { Outlet, createRouteData, useRouteData } from 'solid-start';
+import { NotesProvider } from '~/lib/store/notes';
 
 export function routeData() {
-    return createRouteData(async (_, { fetch }) => {
-        return ["Note 1", "Note 2"];
-    });
+  return createRouteData(async (_, { fetch }) => {
+    return ['Note 1', 'Note 2'];
+  });
 }
 
 export default function Page() {
-    const notes = useRouteData<typeof routeData>(); //this does not work
-    // const [notes] = createSignal(["Note 1", "Note 2"]); //this will work
+  const notes = useRouteData<typeof routeData>(); //this does not work
+  //   const [notes] = createSignal(['Note 1', 'Note 2']); //this will work
 
-    return (
-        <NotesProvider notes={notes()}>
-            <Outlet />
-        </NotesProvider>
-    );
+  return (
+    <NotesProvider notes={notes()}>
+      <Outlet />
+    </NotesProvider>
+  );
 }
